da495dc1f723fe1744d2f347219d1b987188bee4
Creator-Lynx <1_linx_1@mail.ru>
2022-06-03 22:58:57 +0300

    added physics water simulation

---
+++ Assets/Controller/Scripts/WaterSimulation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WaterSimulation : MonoBehaviour
+{
+    [SerializeField] float WaterDensity = 10f;
+
+    [SerializeField] Vector3 forceDirection = Vector3.up;
+    Rigidbody rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float divePercent = -transform.position.y + transform.localScale.x * 0.5f;
+        divePercent = Mathf.Clamp(divePercent, 0f, 1f);
+
+
+        rig.AddForce(forceDirection * divePercent * WaterDensity);
+        rig.drag = divePercent * 2f;
+        rig.angularDrag = divePercent * 2f;
+    }
+}
