3c1d176397abb9889f7950b8ff4325034c7102fb
Creator-Lynx <1_linx_1@mail.ru>
2022-06-20 01:08:58 +0300

    maked starter scene
    start scene have intro text and async menu loader with progress bar
    testing scene loading

--- Assets/Controller/MenuAssets/TextShower.cs
+++ Assets/Controller/MenuAssets/TextShower.cs
@@ -2,28 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TextShower : MonoBehaviour
 {
     [SerializeField] float timeToShowSimbol = 0.1f;
     [TextArea(minLines: 4, maxLines: 8)]
     [SerializeField] string text = "Данный тестовый билд игры является способом исследования различных вариантов управления и поска наиболее интересного посредством выбора одного из вариантов или комбинации оных. \n Я также собираю информацию о ваших выборах и проведенном времени на уровнях для образования выводов.";
-
+    [SerializeField] string sceneNameToLoad = "";
     TextMeshProUGUI textComponent;
+    AudioSource audioSource;
+    [SerializeField] Image progressBar;
+    [SerializeField] GameObject endLoadingText;
+    AsyncOperation sceneLoading;
     void Start()
     {
         textComponent = GetComponent<TextMeshProUGUI>();
+        audioSource = GetComponent<AudioSource>();
         StartCoroutine(TextShow());
+        sceneLoading = SceneManager.LoadSceneAsync(sceneNameToLoad);
+        sceneLoading.allowSceneActivation = false;
     }
 
     IEnumerator TextShow()
     {
         for (int i = 0; i < text.Length; i++)
         {
-            yield return new WaitForSeconds(timeToShowSimbol);
+            yield return new WaitForSeconds(timeToShowSimbol + UnityEngine.Random.Range(0, timeToShowSimbol));
             textComponent.text = textComponent.text + text[i];
-
+            audioSource.Play();
         }
+
+    }
+
+    void Update()
+    {
+        if (Input.anyKey || Input.touchCount > 0)
+            if (sceneLoading != null)
+                sceneLoading.allowSceneActivation = true;
+
+
+        if (progressBar != null)
+        {
+            progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, sceneLoading.progress / 0.9f, 0.1f);
+            if (sceneLoading.progress / 0.9f > 0.9f && endLoadingText != null) endLoadingText.SetActive(true);
+        }
+
 
 
 
