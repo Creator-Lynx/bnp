e2a7834a9eb070d9b539446bc1f7743356395283
Creator-Lynx <1_linx_1@mail.ru>
2022-10-15 16:03:09 +0300

    added FOV controller в зависимости от скорости передвижения персонажа в пределах собственного forward-вектора изменяется величина поля зрения камеры для придания ощущения скорости. Используется дельта локального изменения перемещения за кадр и по нему интерполируется значения FOV от минимального до максимального.

---
+++ Assets/Controller/Scripts/CameraFOVcontroller.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class CameraFOVcontroller : MonoBehaviour
+{
+    [SerializeField]
+    float minSqrDelta, maxSqrDelta;
+    [Space(20)]
+    [SerializeField]
+    float minFOV, maxFOV;
+    Vector3 prevPosition;
+    Vector3 prevForward;
+    [SerializeField]
+    Transform target;
+    [Space(40)] public float testSqrDelta;
+    Camera selfCam;
+    void Start()
+    {
+        selfCam = GetComponent<Camera>();
+        prevPosition = target.position;
+        prevForward = target.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 deltaV = target.position - prevPosition;
+        deltaV = Vector3.Project(deltaV, prevForward);
+
+        prevPosition = target.position;
+        prevForward = target.forward;
+
+        float sqrDelta = deltaV.sqrMagnitude;
+        testSqrDelta = sqrDelta;
+        float t = Mathf.InverseLerp(minSqrDelta, maxSqrDelta, sqrDelta);
+        float currentFOV = Mathf.Lerp(minFOV, maxFOV, t);
+        selfCam.fieldOfView = Mathf.Lerp(selfCam.fieldOfView, currentFOV, 0.9f * Time.deltaTime);
+    }
+}
