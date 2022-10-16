553d2edef8e1474178b100a0f2e907b5ae4fdcef
Creator-Lynx <1_linx_1@mail.ru>
2022-10-16 23:28:34 +0300

    added moving obstacle добавлены подвижные препятствия - плавающие ветки добавлен анимирующий движение по воде скрипт и анимация вращения ветки расставлены по карте

---
+++ Assets/Obstacles/Scripts/MovingObstacle.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    [SerializeField]
+    Transform moving, point0, point1;
+    [SerializeField]
+    float timeToNext = 2f;
+    float timer = 0f, k = 1;
+    void Start()
+    {
+        timer = UnityEngine.Random.Range(0f, timeToNext);
+    }
+    void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime * k;
+        float t = Mathf.SmoothStep(0, 1, timer / timeToNext);
+        moving.position = Vector3.Lerp(point0.position, point1.position, t);
+        if (k > 0 && timer > timeToNext) k = -1;
+        if (k < 0 && timer < 0f) k = 1;
+    }
+}
