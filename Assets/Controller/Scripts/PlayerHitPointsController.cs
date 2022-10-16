7fa29147f5f3fa77dbc0977e953709de41010a1d
Creator-Lynx <1_linx_1@mail.ru>
2022-10-16 13:20:33 +0300

    made hp controller

--- Assets/Controller/Scripts/PlayerHitPointsController.cs
+++ Assets/Controller/Scripts/PlayerHitPointsController.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHitPointsController : MonoBehaviour
 {
-    public int HP { get; private set; } = 10;
-    public int Attempts { get; private set; } = 3;
+    public int HitPoints { get => HP; }
+    [SerializeField]
+    int HP = 10;
+    public int MaximumHitPoints { get => maxHP; }
+    [SerializeField]
+    int maxHP = 10;
+    public int AttemptsNumber { get => attempts; }
+    [SerializeField]
+    int attempts = 3;
+    [SerializeField]
+    [Space(30)]
+    HPBar bar;
+    [SerializeField]
+    Image[] attemptsImages = new Image[3];
     void Start()
+    {
+        SetAttempts();
+    }
+
+    void SetAttempts()
+    {
+        for (int i = 0; i < attemptsImages.Length; i++)
+        {
+            if (i >= attempts) attemptsImages[i].color = Color.gray / 2f;
+        }
+    }
+    public void SetDamage(int damage)
+    {
+        HP -= damage;
+        if (HP <= 0)
+        {
+            HP = maxHP;
+            attempts--;
+            SetAttempts();
+            ReturnToCheckPoint();
+        }
+        if (attempts <= 0)
+        {
+            HP = 0;
+            Death();
+        }
+        bar.OnHpChange((float)HP / (float)maxHP);
+
+    }
+    void ReturnToCheckPoint()
     {
 
     }
-
-    // Update is called once per frame
-    void Update()
+    void Death()
     {
 
     }
