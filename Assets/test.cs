03b8d6c67ccbd0dcd1a907c0a601db63c8ae306f
Creator-Lynx <1_linx_1@mail.ru>
2022-11-17 23:02:32 +0300

    testing different deltaTimes

---
+++ Assets/test.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    int framerate = 0;
+    private void Start()
+    {
+        Application.targetFrameRate = framerate;
+    }
+    bool continued = false, once = false;
+    int startFrame = 0;
+    float startTime = 0f;
+    private void Update()
+    {
+
+
+    }
+    private void FixedUpdate()
+    {
+        Moving();
+    }
+
+    void Moving()
+    {
+        if (Time.unscaledTime >= 0.4f && !once)
+        {
+            continued = true;
+            once = true;
+            startFrame = Time.frameCount;
+            startTime = Time.unscaledTime;
+        }
+        if (continued && Time.unscaledTime >= 1.4f)
+        {
+            continued = false;
+            Debug.Log(Time.unscaledTime - startTime);
+            Debug.Log(Time.frameCount - startFrame);
+        }
+        if (continued)
+        {
+            transform.position += Vector3.right * speed * Time.fixedUnscaledDeltaTime;
+            Debug.Log("fixed " + Time.fixedUnscaledDeltaTime +
+                    "\nunfixed" + Time.unscaledDeltaTime);
+        }
+    }
+}
