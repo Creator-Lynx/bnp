8c18ae830958bdc32b02e11e1f5f8fb7dd8b725a
Creator-Lynx <1_linx_1@mail.ru>
2023-03-09 17:15:30 +0300

    handle CP particle via script some changes in bloom

--- Assets/Game management/SavingSystem/CheckPointTrigger.cs
+++ Assets/Game management/SavingSystem/CheckPointTrigger.cs
@@ -6,12 +6,17 @@ public class CheckPointTrigger : MonoBehaviour
 {
     [SerializeField]
     bool isUsed = false;
+    [SerializeField]
+    ParticleSystem spikes, box;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !isUsed)
         {
             SavingManager.MakeSave();
             isUsed = true;
+            box.Play();
+            spikes.TriggerSubEmitter(0);
+            spikes.Stop(true, ParticleSystemStopBehavior.StopEmitting);
         }
     }
 }
