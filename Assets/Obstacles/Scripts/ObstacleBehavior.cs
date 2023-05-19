8fc3895c542472d66262d9ca52d682b833b442e6
Creator-Lynx <1_linx_1@mail.ru>
2023-05-19 15:44:12 +0300

    some small changes of camera curve... and testing changes for recording that will revert (splash duration) comment debug lines more of camera testing

--- Assets/Obstacles/Scripts/ObstacleBehavior.cs
+++ Assets/Obstacles/Scripts/ObstacleBehavior.cs
@@ -7,7 +7,7 @@ public class ObstacleBehavior : MonoBehaviour
         PlayerHitPointsController damageble = other.collider.GetComponentInParent<PlayerHitPointsController>();
         if (damageble)
         {
-            Debug.Log(other.impulse.magnitude + "\n" + other.relativeVelocity.magnitude);
+            //Debug.Log(other.impulse.magnitude + "\n" + other.relativeVelocity.magnitude);
             if (other.relativeVelocity.sqrMagnitude > Mathf.Pow(damageble.impulseDamageTreashold, 2))
                 damageble.SetDamage(1);
         }
