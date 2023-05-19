e2a7de8d623ba6969f59a06e48e8198850fd689b
Creator-Lynx <1_linx_1@mail.ru>
2023-05-20 02:51:53 +0300

    saving variant of camera mover limitation by height

--- Assets/GameCurves/CameraCurveMover.cs
+++ Assets/GameCurves/CameraCurveMover.cs
@@ -24,6 +24,14 @@ public class CameraCurveMover : MonoBehaviour
         rotatedObject.localEulerAngles = new Vector3(rotatedObject.localEulerAngles.x, rotatedObject.localEulerAngles.y, 0f);
         rotatedObject.transform.position = Vector3.Lerp(rotatedObject.position, springFollower.position, springFollowerLerpRate * Time.deltaTime);
     }
+
+
+    [Header("Модификации скорости от изменения высоты")]
+    [Space(30)]
+    [SerializeField]
+    float maxHeightDelta = 15f;
+    [SerializeField]
+    float minHeightDelta = 8f, lerpSpeedModifierRange = 0.5f;
     void SelfMoving()
     {
         Vector3 dir = curve.GetDirectionByT(t);
@@ -31,9 +39,12 @@ public class CameraCurveMover : MonoBehaviour
         modifiedTargetPos.y = curve.GetPointByT(t).y;
         Vector3 tptr = modifiedTargetPos - curve.GetPointByT(t);
         float dotDist = Vector3.Dot(tptr, dir);
+        //offset modifying
+        float heightDelta = modifiedTargetPos.y - target.position.y;
+        float lerpSpeedModifier = Mathf.Lerp(1, lerpSpeedModifierRange, Mathf.InverseLerp(minHeightDelta, maxHeightDelta, heightDelta));
         if (dotDist > offset)
         {
-            t = Mathf.Lerp(t, t + (findStep / curve.SegmentsNumber), Time.deltaTime * lerpSpeed);
+            t = Mathf.Lerp(t, t + (findStep / curve.SegmentsNumber), Time.deltaTime * lerpSpeed * lerpSpeedModifier);
         }
 
         //transform.position = curve.GetPointByT(t) + Vector3.back * 6f + Vector3.up * 4f;
