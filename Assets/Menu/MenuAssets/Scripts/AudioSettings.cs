cf57c49a9b55728d814fcfbf88be6828fe42fc6a
Creator-Lynx <1_linx_1@mail.ru>
2023-05-21 15:49:56 +0300

    added playerPrefs values setting to slider. treaks and fix

--- Assets/Menu/MenuAssets/Scripts/AudioSettings.cs
+++ Assets/Menu/MenuAssets/Scripts/AudioSettings.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
-
+    [SerializeField]
+    Scrollbar masterSlider, musicSlider, sfxSlider, uiSlider, ambientSlider;
     void Start()
     {
-        mixer.SetFloat("musicVolume", PlayerPrefs.GetFloat("musicVolume", 0));
-        mixer.SetFloat("sfxVolume", PlayerPrefs.GetFloat("sfxVolume", 0));
-        mixer.SetFloat("masterVolume", PlayerPrefs.GetFloat("masterVolume", 0));
-        mixer.SetFloat("uiVolume", PlayerPrefs.GetFloat("uiVolume", 0));
-        mixer.SetFloat("ambientVolume", PlayerPrefs.GetFloat("ambientVolume", 0));
+        float[] data = new float[5];
+
+        data[0] = PlayerPrefs.GetFloat("musicVolume", 0);
+        musicSlider.value = ConvertToDB(data[0]);
+        mixer.SetFloat("musicVolume", data[0]);
+
+        data[1] = PlayerPrefs.GetFloat("sfxVolume", 0);
+        sfxSlider.value = ConvertToDB(data[1]);
+        mixer.SetFloat("sfxVolume", data[1]);
+
+        data[2] = PlayerPrefs.GetFloat("masterVolume", 0);
+        masterSlider.value = ConvertToDB(data[2]);
+        mixer.SetFloat("masterVolume", data[2]);
+
+        data[3] = PlayerPrefs.GetFloat("uiVolume", 0);
+        uiSlider.value = ConvertToDB(data[3]);
+        mixer.SetFloat("uiVolume", data[3]);
+
+        data[4] = PlayerPrefs.GetFloat("ambientVolume", 0);
+        ambientSlider.value = ConvertToDB(data[4]);
+        mixer.SetFloat("ambientVolume", data[4]);
 
     }
 
@@ -21,39 +39,42 @@ public class AudioSettings : MonoBehaviour
     AudioMixer mixer;
     [SerializeField]
     float soundsVolumeMultiplier = 80;
+    [SerializeField]
+    float overZeroDBVolume = 5;
 
+    float ConvertToDB(float value, bool isForward = true)
+    {
+        if (isForward)
+            return value == 0 ? -80 : (value * (soundsVolumeMultiplier + overZeroDBVolume)) - soundsVolumeMultiplier; // convert to DB
+        else return value == -80 ? 0 : (value + soundsVolumeMultiplier) / (soundsVolumeMultiplier + overZeroDBVolume); //convert to 0..1
+    }
     public void OnChangedMusicSlider(float value)
     {
-        float volumeSetting = value; // 0...1
-        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        float vol = ConvertToDB(value);
         mixer.SetFloat("musicVolume", vol);
         PlayerPrefs.SetFloat("musicVolume", vol);
     }
     public void OnChangedSFXSlider(float value)
     {
-        float volumeSetting = value; // 0...1
-        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        float vol = ConvertToDB(value);
         mixer.SetFloat("sfxVolume", vol);
         PlayerPrefs.SetFloat("sfxVolume", vol);
     }
     public void OnChangedMasterSlider(float value)
     {
-        float volumeSetting = value; // 0...1
-        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        float vol = ConvertToDB(value);
         mixer.SetFloat("masterVolume", vol);
         PlayerPrefs.SetFloat("masterVolume", vol);
     }
     public void OnChangedUISlider(float value)
     {
-        float volumeSetting = value; // 0...1
-        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        float vol = ConvertToDB(value);
         mixer.SetFloat("uiVolume", vol);
         PlayerPrefs.SetFloat("uiVolume", vol);
     }
     public void OnChangedAmbientSlider(float value)
     {
-        float volumeSetting = value; // 0...1
-        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        float vol = ConvertToDB(value);
         mixer.SetFloat("ambientVolume", vol);
         PlayerPrefs.SetFloat("ambientVolume", vol);
     }
