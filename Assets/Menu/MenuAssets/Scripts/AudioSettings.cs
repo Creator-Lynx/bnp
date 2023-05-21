23ebf31777d8cdb5c08d124567295fe624a7dd7e
Creator-Lynx <1_linx_1@mail.ru>
2023-05-21 16:31:25 +0300

    added compressor to ui. slider set wothout event made more smooth expiriens in menu when sound settings is loud

--- Assets/Menu/MenuAssets/Scripts/AudioSettings.cs
+++ Assets/Menu/MenuAssets/Scripts/AudioSettings.cs
@@ -9,31 +9,34 @@ public class AudioSettings : MonoBehaviour
 {
     [SerializeField]
     Scrollbar masterSlider, musicSlider, sfxSlider, uiSlider, ambientSlider;
-    void Start()
+    void Awake()
     {
         float[] data = new float[5];
 
         data[0] = PlayerPrefs.GetFloat("musicVolume", 0);
-        musicSlider.value = ConvertToDB(data[0], false);
+        musicSlider.SetValueWithoutNotify(ConvertToDB(data[0], false));
         mixer.SetFloat("musicVolume", data[0]);
 
         data[1] = PlayerPrefs.GetFloat("sfxVolume", 0);
-        sfxSlider.value = ConvertToDB(data[1], false);
+        sfxSlider.SetValueWithoutNotify(ConvertToDB(data[1], false));
         mixer.SetFloat("sfxVolume", data[1]);
 
         data[2] = PlayerPrefs.GetFloat("masterVolume", 0);
-        masterSlider.value = ConvertToDB(data[2], false);
+        masterSlider.SetValueWithoutNotify(ConvertToDB(data[2], false));
         mixer.SetFloat("masterVolume", data[2]);
 
         data[3] = PlayerPrefs.GetFloat("uiVolume", 0);
-        uiSlider.value = ConvertToDB(data[3], false);
+        uiSlider.SetValueWithoutNotify(ConvertToDB(data[3], false));
         mixer.SetFloat("uiVolume", data[3]);
 
+
         data[4] = PlayerPrefs.GetFloat("ambientVolume", 0);
-        ambientSlider.value = ConvertToDB(data[4], false);
+        ambientSlider.SetValueWithoutNotify(ConvertToDB(data[4], false));
         mixer.SetFloat("ambientVolume", data[4]);
 
     }
+
+
 
     [SerializeField]
     AudioMixer mixer;
