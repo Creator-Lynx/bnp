14d03993cdfbbf86137b3cf7f37fb18c677102fa
Creator-Lynx <1_linx_1@mail.ru>
2023-05-21 07:00:04 +0300

    completed audio settings script

--- Assets/Menu/MenuAssets/Scripts/AudioSettings.cs
+++ Assets/Menu/MenuAssets/Scripts/AudioSettings.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        mixer.SetFloat("musicVolume", PlayerPrefs.GetFloat("musicVolume"));
+        mixer.SetFloat("sfxVolume", PlayerPrefs.GetFloat("sfxVolume"));
+        mixer.SetFloat("masterVolume", PlayerPrefs.GetFloat("masterVolume"));
+        mixer.SetFloat("uiVolume", PlayerPrefs.GetFloat("uiVolume"));
+        mixer.SetFloat("ambientVolume", PlayerPrefs.GetFloat("ambientVolume"));
+
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    AudioMixer mixer;
+    [SerializeField]
+    float soundsVolumeMultiplier = 80;
+
+    public void OnChangedMusicSlider(float value)
     {
-        
+        float volumeSetting = value; // 0...1
+        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        mixer.SetFloat("musicVolume", vol);
+        PlayerPrefs.SetFloat("musicVolume", vol);
+    }
+    public void OnChangedSFXSlider(float value)
+    {
+        float volumeSetting = value; // 0...1
+        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        mixer.SetFloat("sfxVolume", vol);
+        PlayerPrefs.SetFloat("sfxVolume", vol);
+    }
+    public void OnChangedMasterSlider(float value)
+    {
+        float volumeSetting = value; // 0...1
+        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        mixer.SetFloat("masterVolume", vol);
+        PlayerPrefs.SetFloat("masterVolume", vol);
+    }
+    public void OnChangedUISlider(float value)
+    {
+        float volumeSetting = value; // 0...1
+        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        mixer.SetFloat("uiVolume", vol);
+        PlayerPrefs.SetFloat("uiVolume", vol);
+    }
+    public void OnChangedAmbientSlider(float value)
+    {
+        float volumeSetting = value; // 0...1
+        float vol = volumeSetting == 0 ? -80 : (volumeSetting * (soundsVolumeMultiplier + 10)) - soundsVolumeMultiplier; // convert to DB
+        mixer.SetFloat("ambientVolume", vol);
+        PlayerPrefs.SetFloat("ambientVolume", vol);
     }
 }
